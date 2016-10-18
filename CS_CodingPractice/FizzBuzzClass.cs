28299b24ad8aee1bf99fe2b0f31b04f60f9b30d5
Jose Torres <xaerofox@gmail.com>
2016-10-18 14:31:48 -0400

    FizzBuzz Class is now functional

--- CS_CodingPractice/FizzBuzzClass.cs
+++ CS_CodingPractice/FizzBuzzClass.cs
@@ -11,8 +11,7 @@ namespace CS_CodingPractice
     * running up to but not including end, so for example start = 1 and end = 5 gives the series 1, 2, 3, 4. 
     * Return a new String[] array containing the string form of these numbers, except for multiples of 3, use "Fizz" 
     * instead of the number, for multiples of 5 use "Buzz", and for multiples of both 3 and 5 use "FizzBuzz". 
-    * In Java, String.valueOf(xxx) will make the String form of an int or other type.This version is a little more 
-    * complicated than the usual version since you have to allocate and index into an array instead of just printing, 
+    * This version is a little more complicated than the usual version since you have to allocate and index into an array instead of just printing, 
     * and we vary the start/end instead of just always doing 1..100. 
     * 
     * fizzBuzz(1, 6) → ["1", "2", "Fizz", "4", "Buzz"]
@@ -24,7 +23,40 @@ namespace CS_CodingPractice
     {
         static void Main(string[] args)
         {
+            string[] fbArr;
 
+
+            fizzBuzzModified(1, 6);
+            fizzBuzzModified(1, 8);
+            fizzBuzzModified(1, 11);
+            
+
+            fbArr = fizzBuzzModified(36, 90);
+
+            for (int i = 0; i < fbArr.Length; i++)
+            {
+                Console.WriteLine(fbArr[i]);
+            }
+        }
+
+        public static string[] fizzBuzzModified(int start, int end)
+        {
+            string[] fbArray = new string[end - start];
+            for (int i = 0; i < fbArray.Length; i++)
+            {
+                if (start % 3 == 0 && start % 5 == 0)
+                    fbArray[i] = "FizzBuzz";
+                else if (start % 3 == 0)
+                    fbArray[i] = "Fizz";
+                else if (start % 5 == 0)
+                    fbArray[i] = "Buzz";
+                else
+                    fbArray[i] = start.ToString();
+
+                start++;
+            }
+
+            return fbArray;
         }
 
 
